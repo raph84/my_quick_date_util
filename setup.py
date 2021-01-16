39153037ecc3e3cb8c9688a49a765e33de8b3345
raph84 <raph.berube@gmail.com>
2021-01-16 13:53:09 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='my_quick_date_util',
+      version='0.1',
+      description='Quick date functions for my location',
+      url='http://github.com/raph84/my_quick_date_util',
+      author='raph84',
+      author_email='raph.berube@gmail.com',
+      license='GNU GPLv3',
+      packages=['my_quick_date_util'],
+      install_requires=[
+          'pytz',
+      ],
+      zip_safe=False)
